cdb731ddc6745261768bed1f2c98e2051a6a2bfa
hakimo <achache.hakim@gmail.com>
2023-10-01 12:57:04 +0200

    last change

--- src/Form/ProductsFormType.php
+++ src/Form/ProductsFormType.php
@@ -9,7 +9,6 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
